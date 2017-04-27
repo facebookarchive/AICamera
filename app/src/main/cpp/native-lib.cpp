f5a7ff22a74b3344b9a272e70a40b0195ade1a77
Bram Wasti <bwasti@fb.com>
2017-04-27 09:50:05 -0700

    Subtract imagenet mean to increase prediction accuracy.  Clarify RGB->BGR by correcting names.  Fixes #13

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -96,23 +96,27 @@ Java_facebook_f8demo_ClassifyCamera_classificationFromCaffe2(
             char u = U_row[2 * ((w_offset+j)/2)];
             char v = V_row[2 * ((w_offset+j)/2)];
 
-            auto r_i = 0 * IMG_H * IMG_W + j * IMG_W + i;
+            float b_mean = 104.00698793f;
+            float g_mean = 116.66876762f;
+            float r_mean = 122.67891434f;
+
+            auto b_i = 0 * IMG_H * IMG_W + j * IMG_W + i;
             auto g_i = 1 * IMG_H * IMG_W + j * IMG_W + i;
-            auto b_i = 2 * IMG_H * IMG_W + j * IMG_W + i;
+            auto r_i = 2 * IMG_H * IMG_W + j * IMG_W + i;
 
             if (infer_HWC) {
-                r_i = (j * IMG_W + i) * IMG_C;
+                b_i = (j * IMG_W + i) * IMG_C;
                 g_i = (j * IMG_W + i) * IMG_C + 1;
-                b_i = (j * IMG_W + i) * IMG_C + 2;
+                r_i = (j * IMG_W + i) * IMG_C + 2;
             }
 /*
   R = Y + 1.402 (V-128)
   G = Y - 0.34414 (U-128) - 0.71414 (V-128)
   B = Y + 1.772 (U-V)
  */
-            input_data[b_i] = (float) ((float) min(255., max(0., (float) (y + 1.402 * (v - 128)))));
-            input_data[g_i] = (float) ((float) min(255., max(0., (float) (y - 0.34414 * (u - 128) - 0.71414 * (v - 128)))));
-            input_data[r_i] = (float) ((float) min(255., max(0., (float) (y + 1.772 * (u - v)))));
+            input_data[r_i] = -r_mean + (float) ((float) min(255., max(0., (float) (y + 1.402 * (v - 128)))));
+            input_data[g_i] = -g_mean + (float) ((float) min(255., max(0., (float) (y - 0.34414 * (u - 128) - 0.71414 * (v - 128)))));
+            input_data[b_i] = -b_mean + (float) ((float) min(255., max(0., (float) (y + 1.772 * (u - v)))));
 
         }
     }
